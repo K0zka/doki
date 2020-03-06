4ee1fedaa8981f3e45b1d99bef5925c3ddf78170
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-03-06 19:58:59 +0100

    junit data model

--- benchmarx-model/src/main/kotlin/io/github/k0zka/benchmarx/model/junit/JunitTestCase.kt
+++ benchmarx-model/src/main/kotlin/io/github/k0zka/benchmarx/model/junit/JunitTestCase.kt
@@ -1,3 +1,4 @@
 package io.github.k0zka.benchmarx.model.junit
 
-data class JunitTestCase(val name: String, val time : Double)
+data class JunitTestCase(val name: String, val time: Double, val message: String, val excType: String,
+						 val trace: String, val outcome: Outcome)
---
+++ benchmarx-model/src/main/kotlin/io/github/k0zka/benchmarx/model/junit/Outcome.kt
@@ -0,0 +1,7 @@
+package io.github.k0zka.benchmarx.model.junit
+
+enum class Outcome {
+	PASS,
+	FAIL,
+	IGNORE
+}
