4c94969ac27bd20711058821b6755b35b826a883
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2020-06-24 07:56:08 +0200

    scenario added

--- benchmarx-integration-tests/src/test/kotlin/io/github/k0zka/benchmarx/tests/ApplicationSteps.kt
+++ benchmarx-integration-tests/src/test/kotlin/io/github/k0zka/benchmarx/tests/ApplicationSteps.kt
@@ -1,11 +1,21 @@
 package io.github.k0zka.benchmarx.tests
 
 import io.cucumber.java8.En
+import io.cucumber.java8.Scenario
 
 class ApplicationSteps : En {
+
+	lateinit var scenario: Scenario
+
 	init {
+
+		Before {
+			scenario ->
+			this.scenario = scenario
+		}
+
 		Given("a running benchmarx app") {
-			TODO()
+
 		}
 	}
 }
