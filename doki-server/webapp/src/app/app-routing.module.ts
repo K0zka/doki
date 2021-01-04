1a8a9c15cc09f9ea58577049acb5746c74aebcb4
Laszlo Hornyak <laszlo.hornyak@gmail.com>
2021-01-04 21:21:32 +0100

    routes

--- doki-server/webapp/src/app/app-routing.module.ts
+++ doki-server/webapp/src/app/app-routing.module.ts
@@ -5,14 +5,21 @@ import { NewProjectComponent } from './new-project/new-project.component'
 import { ProjectComponent } from './project/project.component'
 import { JmhReportComponent } from './jmh-report/jmh-report.component'
 import { GatlingReportComponent } from './gatling-report/gatling-report.component'
+import { CucumberReportComponent } from './cucumber-report/cucumber-report.component'
+import { JunitReportComponent } from './junit-report/junit-report.component'
 import { PageNotFoundComponent } from './page-not-found/page-not-found.component'
 
 const routes: Routes = [
   { path: '', component: StartpageComponent },
   { path: 'new-project', component: NewProjectComponent },
-  { path: 'project', component: ProjectComponent },
-  { path: 'project/**/jmh', component: JmhReportComponent },
-  { path: 'project/**/gatling', component: GatlingReportComponent },
+  { path: 'projects', component: ProjectComponent },
+  { path: 'projects/:projectId/junit', component: JunitReportComponent },
+  { path: 'projects/:projectId/junit/:testCase', component: JunitReportComponent }, // TODO
+  { path: 'projects/:projectId/cucumber', component: CucumberReportComponent },
+  { path: 'projects/:projectId/cucumber/:scenario', component: CucumberReportComponent }, // TODO
+  { path: 'projects/:projectId/jmh', component: JmhReportComponent },
+  { path: 'projects/:projectId/jmh/:benchmark', component: JmhReportComponent }, // TODO
+  { path: 'projects/:projectId/gatling', component: GatlingReportComponent },
 
   { path: '**', component: PageNotFoundComponent }
 ]
